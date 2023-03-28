b5e60d4d67222a3e1a6f1ce324184d6656a0c22f
Cobos Melgares external <sancobosm@gmail.com>
2023-03-28 15:38:28 +0200

    Primer commit

---
+++ src/main/java/ldsp/app/LaDamaYSusPeonesApplication.java
@@ -0,0 +1,13 @@
+package ldsp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaDamaYSusPeonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaDamaYSusPeonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ldsp/app/LaDamaYSusPeonesApplicationTests.java
@@ -0,0 +1,13 @@
+package ldsp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaDamaYSusPeonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
